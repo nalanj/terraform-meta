94cc96d69af53766007be16eab5483c3dbb670af
Alan Johnson <nalanj@fastmail.com>
2023-02-18 08:25:03 -0500

    Set org on oauth and see if that helps

--- oauth.tf
+++ oauth.tf
@@ -1,5 +1,6 @@
 resource "tfe_oauth_client" "github_client" {
   name             = "github_oauth_client"
+  organization     = "nalanj"
   api_url          = "https://api.github.com"
   http_url         = "https://github.com"
   oauth_token      = var.tfe_github_token
